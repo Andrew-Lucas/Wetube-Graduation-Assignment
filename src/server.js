c505e44a5bc14a684201bb8ee9984da5a89a7f29
Andrew Lucas <113585213+Andrew-Lucas@users.noreply.github.com>
2022-11-20 15:04:42 +0300

    INTRODUCTION TO EXPRESS

--- src/server.js
+++ src/server.js
@@ -4,9 +4,27 @@ const app = express()
 
 const PORT = 4000
 
-const serverRespond = ()=> console.log("Someone is tryng to acces the server")
+const serverRespond = (req, res)=> {
+  console.log(res)
+  return res.send("<h1>Response from the server</h1>")
+}
 app.get("/", serverRespond)
 
-const serverListen = ()=> console.log(`Server listening from http://localhost:${PORT}`)
 
-app.listen(4000, serverListen)
+const testMiddleWare1 = (req, res, next)=>{
+  console.log("First middle ware")
+  return next()
+}
+const testMiddleWare2 = (req, res, next)=>{
+  console.log("Second middle ware")
+  return next()
+}
+const testServerResponse = (req, res)=>{
+  console.log("Server Response Returned")
+  return res.send("<h2>Test Server Response</h2>")
+}
+app.get("/testserver", testMiddleWare1, testMiddleWare2 ,testServerResponse)
+
+
+const serverListen = ()=> console.log(`Server listening from http://localhost:${PORT}`)
+app.listen(PORT, serverListen) 
