4d3719dcece9813f4fe766b77bb5e6bd6f8073bd
Andrew Lucas <113585213+Andrew-Lucas@users.noreply.github.com>
2022-11-21 18:20:13 +0300

    Completed EXPRESS

--- src/server.js
+++ src/server.js
@@ -1,30 +1,39 @@
 import express from "express"
+import morgan from "morgan"
 
+const logger = morgan("dev")
 const app = express()
 
 const PORT = 4000
 
 const serverRespond = (req, res)=> {
-  console.log(res)
   return res.send("<h1>Response from the server</h1>")
 }
 app.get("/", serverRespond)
 
 
-const testMiddleWare1 = (req, res, next)=>{
+/* const testMiddleWare1 = (req, res, next)=>{
   console.log("First middle ware")
   return next()
 }
 const testMiddleWare2 = (req, res, next)=>{
-  console.log("Second middle ware")
-  return next()
-}
+  const url = req.url
+  if(url === "/testserver/a"){
+    console.log("❌Page not allowed")
+    return res.send("Bro your lost")
+  } else{
+    console.log(req.path)
+    return next()
+  }
+} */
+
 const testServerResponse = (req, res)=>{
   console.log("Server Response Returned")
   return res.send("<h2>Test Server Response</h2>")
 }
-app.get("/testserver", testMiddleWare1, testMiddleWare2 ,testServerResponse)
+app.use(logger)
+app.get("/testserver", testServerResponse)
 
 
-const serverListen = ()=> console.log(`Server listening from http://localhost:${PORT}`)
+const serverListen = ()=> console.log(`✅Server listening from http://localhost:${PORT}`)
 app.listen(PORT, serverListen) 
